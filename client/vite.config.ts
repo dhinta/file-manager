b7b3b32fcb49b4bc0edcc5dfbc69886a2121172d
Debasish <relaxed.dhinta@gmil.com>
2024-08-21 23:13:32 +0530

    tailwind added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 80,
-  },
+    plugins: [react()],
+    server: {
+        port: 80,
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    },
 });
